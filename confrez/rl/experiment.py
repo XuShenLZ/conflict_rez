d3b8f090e081e6f8e63ab14f3b0593cd1f78b57c
BARC gp node <thomasfork@gmail.com>
2022-07-28 23:28:50 -0700

    added DQN

--- confrez/rl/experiment.py
+++ confrez/rl/experiment.py
@@ -1,11 +1,12 @@
 from confrez.rl.pklot_env import parallel_env
 from confrez.rl.utils import ProcessMonitor
-from stable_baselines3 import PPO
+from stable_baselines3 import DQN, PPO
 
 env = parallel_env()
 monitor = ProcessMonitor(env)
 
-model = PPO.load("policy")
+# model = PPO.load("PPO-CNN_07-28-2022_12-35-52")
+model = DQN.load("DQN-CNN_07-28-2022_16-40-09")
 
 observations = env.reset(seed=0)
 env.render()
--- confrez/rl/train.py
+++ confrez/rl/train.py
@@ -1,7 +1,8 @@
 from confrez.rl.pklot_env import parallel_env
 import supersuit as ss
-from stable_baselines3 import PPO
-from stable_baselines3.ppo import CnnPolicy
+from stable_baselines3 import PPO, DQN
+
+# from stable_baselines3.ppo import CnnPolicy
 
 from datetime import datetime
 from os import path as os_path
@@ -17,14 +18,23 @@ env = ss.concat_vec_envs_v1(env, 16, num_cpus=8, base_class="stable_baselines3")
 now = datetime.now()
 timestamp = now.strftime("%m-%d-%Y_%H-%M-%S")
 
-model = PPO(
-    CnnPolicy,
+# model = PPO(
+#     CnnPolicy,
+#     env,
+#     verbose=3,
+#     n_steps=64,
+#     n_epochs=5,
+#     tensorboard_log=f"{cwd}/PPO-CNN_tensorboard/",
+# )
+
+model = DQN(
+    "CnnPolicy",
     env,
     verbose=3,
-    n_steps=64,
-    n_epochs=5,
-    tensorboard_log=f"{cwd}/PPO-CNN_tensorboard/",
+    buffer_size=100000,
+    learning_starts=500,
+    tensorboard_log=f"{cwd}/DQN-CNN_tensorboard/",
 )
 
-model.learn(total_timesteps=2000000, tb_log_name=f"PPO-CNN_{timestamp}")
-model.save(f"PPO-CNN_{timestamp}")
+model.learn(total_timesteps=2000000, tb_log_name=f"DQN-CNN_{timestamp}")
+model.save(f"DQN-CNN_{timestamp}")
