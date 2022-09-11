4d586b03299ec384cc2001ddcc9383b907f4fcec
tianyue-berkeley <tianyue.cheng@berkeley.edu>
2022-09-11 00:06:17 -0700

    added a large feature extractor, waiting to be tested

--- confrez/rl/train.py
+++ confrez/rl/train.py
@@ -1,24 +1,33 @@
 from typing import Callable, List
 from confrez.rl.pklot_env import parallel_env
 import supersuit as ss
+import torch
+from torch import nn, tensor
 from stable_baselines3 import PPO, DQN
+from stable_baselines3.dqn import CnnPolicy
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
+from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
 from scipy import interpolate
 
 from datetime import datetime
 from os import path as os_path
 
-#wandb
+wandb
 import wandb
 from wandb.integration.sb3 import WandbCallback
 run = wandb.init(project="rl-parking", 
                 entity="chengtianyue",
-                sync_tensorboard=True,
-                save_code=True
+                sync_tensorboard=True
                 )
 
 cwd = os_path.dirname(__file__)
 
-MODEL_NAME = "DQN-CNN-4v-mixed"
+MODEL_NAME = "DQN-Resnet-Extractor"
 
 env = parallel_env(n_vehicles=4, seed=1, random_reset=True)
 env = ss.black_death_v3(env)
@@ -56,10 +65,53 @@ def step_schedule(
 
     return func
 
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=True)
+
+#https://www.kaggle.com/code/kwabenantim/gfootball-stable-baselines3
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.relu = nn.ReLU()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.conv2 = conv3x3(out_channels, out_channels, stride)
+        
+    def forward(self, x):
+        residual = x
+        out = self.relu(x)
+        out = self.conv1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out += residual
+        return out
+    
+class ParkingCNN(BaseFeaturesExtractor):
+    def __init__(self, observation_space, features_dim=256):
+        super().__init__(observation_space, features_dim)
+        in_channels = observation_space.shape[0]  # channels x height x width
+        self.cnn = nn.Sequential(
+            conv3x3(in_channels=in_channels, out_channels=32),
+            nn.MaxPool2d(kernel_size=3, stride=2, dilation=1, ceil_mode=False),
+            ResidualBlock(in_channels=32, out_channels=32),
+            ResidualBlock(in_channels=32, out_channels=32),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+        self.linear = nn.Sequential(
+          nn.Linear(in_features=152352, out_features=features_dim, bias=True),
+          nn.ReLU(),
+        )
+
+    def forward(self, obs):
+        return self.linear(self.cnn(obs))
+
+policy_kwargs = dict(features_extractor_class=ParkingCNN,
+                     features_extractor_kwargs=dict(features_dim=256))
 
 model = DQN(
-    "CnnPolicy",
+    CnnPolicy,
     env,
+    policy_kwargs=policy_kwargs,
     learning_rate=step_schedule(0.0005, [1, 0.8, 0.6, 0.3], [1, 0.5, 0.1, 0.05]),
     verbose=3,
     buffer_size=100000,
