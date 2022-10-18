96d19207c55d7f1ef3c488706cc7d79576a5ce48
tianyue-berkeley <tianyue.cheng@berkeley.edu>
2022-10-18 09:16:00 -0700

    temporary update, still buggy, needed for fixing

---
+++ confrez/rl/maddpg_train.py
@@ -0,0 +1,98 @@
+import numpy as np
+from typing import Callable, List
+from confrez.rl.pklot_env import parallel_env
+import supersuit as ss
+from maddpg import MADDPG
+from multiagent_buffer import MultiAgentReplayBuffer
+
+from datetime import datetime
+from os import path as os_path
+
+def obs_list_to_state_vector(observation):
+    state = np.array([])
+    for obs in observation:
+        state = np.concatenate([state, obs])
+    return state
+
+if __name__ == '__main__':
+    #scenario = 'simple'
+    # scenario = 'simple_adversary'
+    # env = make_env(scenario)
+    # n_agents = env.n
+    MODEL_NAME = "DQN-Resnet-Extractor"
+    # LOG_DIR = "./log_for_wandb/"
+
+    env = parallel_env(n_vehicles=4, seed=1, random_reset=True)
+    env = ss.black_death_v3(env)
+    env = ss.resize_v1(env, 140, 140)
+    # env = Monitor(env, LOG_DIR) # For plotting use
+    # env = ss.color_reduction_v0(env, mode="B")
+    # env = ss.frame_stack_v2(env, 3)
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+    env = ss.concat_vec_envs_v1(env, 24, num_cpus=12, base_class="stable_baselines3")
+    n_agents = env.n
+    now = datetime.now()
+    timestamp = now.strftime("%m-%d-%Y_%H-%M-%S")
+    actor_dims = []
+    for i in range(n_agents):
+        actor_dims.append(env.observation_space[i].shape[0])
+    critic_dims = sum(actor_dims)
+
+    # action space is a list of arrays, assume each agent has same action space
+    n_actions = env.action_space[0].n
+    maddpg_agents = MADDPG(actor_dims, critic_dims, n_agents, n_actions, 
+                           fc1=64, fc2=64,  
+                           alpha=0.01, beta=0.01, scenario=scenario,
+                           chkpt_dir='tmp/maddpg/')
+
+    memory = MultiAgentReplayBuffer(1000000, critic_dims, actor_dims, 
+                        n_actions, n_agents, batch_size=1024)
+
+    PRINT_INTERVAL = 500
+    N_GAMES = 50000
+    MAX_STEPS = 25
+    total_steps = 0
+    score_history = []
+    evaluate = False
+    best_score = 0
+
+    if evaluate:
+        maddpg_agents.load_checkpoint()
+
+    for i in range(N_GAMES):
+        obs = env.reset()
+        score = 0
+        done = [False]*n_agents
+        episode_step = 0
+        while not any(done):
+            if evaluate:
+                env.render()
+                #time.sleep(0.1) # to slow down the action for the video
+            actions = maddpg_agents.choose_action(obs)
+            obs_, reward, done, info = env.step(actions)
+
+            state = obs_list_to_state_vector(obs)
+            state_ = obs_list_to_state_vector(obs_)
+
+            if episode_step >= MAX_STEPS:
+                done = [True]*n_agents
+
+            memory.store_transition(obs, state, actions, reward, obs_, state_, done)
+
+            if total_steps % 100 == 0 and not evaluate:
+                maddpg_agents.learn(memory)
+
+            obs = obs_
+
+            score += sum(reward)
+            total_steps += 1
+            episode_step += 1
+
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+        if not evaluate:
+            if avg_score > best_score:
+                maddpg_agents.save_checkpoint()
+                best_score = avg_score
+        if i % PRINT_INTERVAL == 0 and i > 0:
+            print('episode', i, 'average score {:.1f}'.format(avg_score))
