f566914e60fbf400af34db51c61376a777dbe87e
Tianyue Cheng <tianyue.cheng@berkeley.edu>
2023-01-11 15:58:31 -0800

    Env bugs solved

--- confrez/rl/test.py
+++ confrez/rl/test.py
@@ -1,103 +1,97 @@
+"""This is a minimal example of using Tianshou with MARL to train agents.
+
+Author: Will (https://github.com/WillDudley)
+
+Python version used: 3.8.10
+
+Requirements:
+pettingzoo == 1.22.0
+git+https://github.com/thu-ml/tianshou
+"""
+
 import os
-from typing import Callable, List, Optional, Tuple
-import pklot_env
-from pklot_env import parallel_env
-import supersuit as ss
-from datetime import datetime
-from os import path as os_path
-import torch
+from typing import Optional, Tuple
+
 import gym
 import numpy as np
-from torch.utils.tensorboard import SummaryWriter
+import torch
 from tianshou.data import Collector, VectorReplayBuffer
-from tianshou.env import DummyVectorEnv, PettingZooEnv
-
-from pettingzoo.classic import tictactoe_v3
-from tianshou.policy import (
-    BasePolicy,
-    DQNPolicy,
-    MultiAgentPolicyManager,
-    RandomPolicy,
-)
+from tianshou.env import DummyVectorEnv
+from tianshou.env.pettingzoo_env import PettingZooEnv
+from tianshou.policy import BasePolicy, DQNPolicy, MultiAgentPolicyManager, RandomPolicy
 from tianshou.trainer import offpolicy_trainer
 from tianshou.utils.net.common import Net
 
-# import wandb
-# from wandb.integration.sb3 import WandbCallback
-# run = wandb.init(project="rl-parking", 
-#                 entity="chengtianyue",
-#                 monitor_gym=True,
-#                 sync_tensorboard=True
-#                 )
+from pettingzoo.classic import tictactoe_v3
 
-cwd = os_path.dirname(__file__)
-now = datetime.now()
-timestamp = now.strftime("%m-%d-%Y_%H-%M-%S")
 
-MODEL_NAME = "Tianshou-multiagent"
-# LOG_DIR = "./log_for_wandb/"
-NUM_AGENT = 4
-
-def get_env():
-    """This function is needed to provide callables for DummyVectorEnv."""
-    env = parallel_env(n_vehicles=4, seed=1, random_reset=False)
-    env = ss.black_death_v3(env)
-    env = ss.resize_v1(env, 140, 140)
-    return PettingZooEnv(env)
-
-def get_agents(
+def _get_agents(
     agent_learn: Optional[BasePolicy] = None,
     agent_opponent: Optional[BasePolicy] = None,
     optim: Optional[torch.optim.Optimizer] = None,
 ) -> Tuple[BasePolicy, torch.optim.Optimizer, list]:
-    env = get_env()
-    observation_space = env.observation_space
-    agents = []
-    for _ in range(NUM_AGENT):
+    env = _get_env()
+    observation_space = (
+        env.observation_space["observation"]
+        if isinstance(env.observation_space, gym.spaces.Dict)
+        else env.observation_space
+    )
+    if agent_learn is None:
+        # model
         net = Net(
-            state_shape= len(observation_space),
-            action_shape=7, #see pklot_env line 99
+            state_shape=observation_space["observation"].shape
+            or observation_space["observation"].n,
+            action_shape=env.action_space.shape or env.action_space.n,
             hidden_sizes=[128, 128, 128, 128],
             device="cuda" if torch.cuda.is_available() else "cpu",
         ).to("cuda" if torch.cuda.is_available() else "cpu")
         if optim is None:
             optim = torch.optim.Adam(net.parameters(), lr=1e-4)
-        
-        agents.append(DQNPolicy(
+        agent_learn = DQNPolicy(
             model=net,
             optim=optim,
             discount_factor=0.9,
-            estimation_step=100,
+            estimation_step=3,
             target_update_freq=320,
-        ))
+        )
 
+    if agent_opponent is None:
+        agent_opponent = RandomPolicy()
+
+    agents = [agent_opponent, agent_learn]
     policy = MultiAgentPolicyManager(agents, env)
     return policy, optim, env.agents
 
+
+def _get_env():
+    """This function is needed to provide callables for DummyVectorEnv."""
+    return PettingZooEnv(tictactoe_v3.env())
+
+
 if __name__ == "__main__":
-    #https://pettingzoo.farama.org/tutorials/tianshou/intermediate/
+
     # ======== Step 1: Environment setup =========
-    train_env = DummyVectorEnv([get_env for _ in range(10)])
-    test_env = DummyVectorEnv([get_env for _ in range(10)])
-    
+    train_envs = DummyVectorEnv([_get_env for _ in range(10)])
+    test_envs = DummyVectorEnv([_get_env for _ in range(10)])
+
     # seed
-    seed = 42
+    seed = 1
     np.random.seed(seed)
     torch.manual_seed(seed)
+    train_envs.seed(seed)
+    test_envs.seed(seed)
 
     # ======== Step 2: Agent setup =========
-    policy, optim, agents = get_agents()
+    policy, optim, agents = _get_agents()
 
     # ======== Step 3: Collector setup =========
-
-
     train_collector = Collector(
         policy,
-        train_env,
-        VectorReplayBuffer(20_000, len(train_env)),
+        train_envs,
+        VectorReplayBuffer(20_000, len(train_envs)),
         exploration_noise=True,
     )
-    test_collector = Collector(policy, test_env, exploration_noise=True)
+    test_collector = Collector(policy, test_envs, exploration_noise=True)
     # policy.set_eps(1)
     train_collector.collect(n_step=64 * 10)  # batch size * training_num
 
