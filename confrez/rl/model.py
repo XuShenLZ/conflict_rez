fixed model with incorrect shape

confrez/rl/model.py
@@ -14,92 +14,97 @@ import numpy as np
 import torch
 from torch import nn
 import gymnasium as gym
-from tianshou.policy import DQNPolicy
+from tianshou.utils.net.common import Net, MLP
+ModuleType = Type[nn.Module]
 
-Schedule = Callable[[float], float]
-
-class BaseFeaturesExtractor(nn.Module):
-    """
-    Adapted from SB3
-    """
-
-    def __init__(self, observation_space: gym.Space, features_dim: int = 0):
-        super().__init__()
-        assert features_dim > 0
-        self._observation_space = observation_space
-        self._features_dim = features_dim
-
-    @property
-    def features_dim(self) -> int:
-        return self._features_dim
-
-    def forward(self, observations: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError()
-
-class NatureCNN(BaseFeaturesExtractor):
-    """
-    Adapted from SB3
-    CNN from DQN Nature paper:
-        Mnih, Volodymyr, et al.
-        "Human-level control through deep reinforcement learning."
-        Nature 518.7540 (2015): 529-533.
-
-    :param observation_space:
-    :param features_dim: Number of features extracted.
-        This corresponds to the number of unit for the last layer.
-    """
-
-    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 512):
-        super().__init__(observation_space, features_dim)
-        # We assume CxHxW images (channels first)
-        # Re-ordering will be done by pre-preprocessing or wrapper
-        n_input_channels = observation_space.shape[0]
-        self.cnn = nn.Sequential(
-            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
-            nn.ReLU(),
-            nn.Flatten(),
-        )
-
-        # Compute shape by doing one forward pass
-        with torch.no_grad():
-            n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
-
-        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
-
-    def forward(self, observations: torch.Tensor) -> torch.Tensor:
-        return self.linear(self.cnn(observations))
-
-class CNNPolicy(DQNPolicy):
+class CNN_DQN(Net):
     def __init__(self, 
-                model: torch.nn.Module, 
-                optim: torch.optim.Optimizer, 
-                observation_space: gym.spaces.Space,
-                action_space: gym.spaces.Space,
-                lr_schedule: Schedule,
-                net_arch: Optional[List[int]] = None,
-                activation_fn: Type[nn.Module] = nn.ReLU,
-                features_extractor_class: Type[BaseFeaturesExtractor] = NatureCNN,
-                features_extractor_kwargs: Optional[Dict[str, Any]] = None,
-                normalize_images: bool = True,
-                discount_factor: float = 0.99, 
-                estimation_step: int = 1, 
-                target_update_freq: int = 0, 
-                reward_normalization: bool = False, 
-                is_double: bool = True, 
-                clip_loss_grad: bool = False,
-                **kwargs: Any) -> None:
-        super().__init__(model, 
-                        optim, 
-                        discount_factor, 
-                        estimation_step, 
-                        target_update_freq, 
-                        reward_normalization, 
-                        is_double, 
-                        clip_loss_grad, 
-                        **kwargs)
-        
-        
+                state_shape: Union[int, Sequence[int]], 
+                action_shape: Union[int, Sequence[int]] = 0, 
+                hidden_sizes: Sequence[int] = ..., 
+                norm_layer: Optional[ModuleType] = None, 
+                activation: Optional[ModuleType] = nn.ReLU, 
+                device: Union[str, int, torch.device] = "cpu", 
+                softmax: bool = False, 
+                concat: bool = False, 
+                num_atoms: int = 1, 
+                dueling_param: Optional[Tuple[Dict[str, Any], Dict[str, Any]]] = None, 
+                linear_layer: Type[nn.Linear] = nn.Linear) -> None:
+        super().__init__(state_shape, 
+                        action_shape, 
+                        hidden_sizes, 
+                        norm_layer, 
+                        activation, 
+                        device, 
+                        softmax, 
+                        concat, 
+                        num_atoms, 
+                        dueling_param, 
+                        linear_layer)
+        self.device = device
+        self.softmax = softmax
+        self.num_atoms = num_atoms
+        input_dim = int(np.prod(state_shape))
+        action_dim = int(np.prod(action_shape)) * num_atoms
+        if concat:
+            input_dim += action_dim
+        self.use_dueling = dueling_param is not None
+        output_dim = action_dim if not self.use_dueling and not concat else 0
+        # self.model = nn.Sequential(
+        #     input_dim, output_dim, hidden_sizes, norm_layer, activation, device,
+        #     linear_layer
+        # )
+        self.output_dim = self.model.output_dim
+        if self.use_dueling:  # dueling DQN
+            q_kwargs, v_kwargs = dueling_param  # type: ignore
+            q_output_dim, v_output_dim = 0, 0
+            if not concat:
+                q_output_dim, v_output_dim = action_dim, num_atoms
+            q_kwargs: Dict[str, Any] = {
+                **q_kwargs, "input_dim": self.output_dim,
+                "output_dim": q_output_dim,
+                "device": self.device
+            }
+            v_kwargs: Dict[str, Any] = {
+                **v_kwargs, "input_dim": self.output_dim,
+                "output_dim": v_output_dim,
+                "device": self.device
+            }
+            self.Q, self.V = MLP(**q_kwargs), MLP(**v_kwargs)
+            self.output_dim = self.Q.output_dim
+
+    def forward(
+            self,
+            obs: torch.Tensor,
+            state: Any = None,
+            info: Dict[str, Any] = {},
+        ) -> Tuple[torch.Tensor, Any]:
+            """Mapping: obs -> flatten (inside MLP)-> logits."""
+            # print("DEBUG:", obs.type)
+            # obs = obs.reshape((3, 10, 140, 140))
+            obs = torch.from_numpy(obs).float()
+            self.cnn = nn.Sequential(
+                nn.Conv2d(obs.shape[0], 32, kernel_size=8, stride=4, padding=0),
+                nn.ReLU(),
+                nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+                nn.ReLU(),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+                nn.ReLU(),
+                nn.Flatten(),
+            )
+            with torch.no_grad():
+                n_flatten = self.cnn(torch.as_tensor(obs).float()).shape[1]
+            self.linear = nn.Sequential(nn.Linear(n_flatten, 7), nn.ReLU())
+            logits = self.linear(self.cnn(obs))
+            bsz = logits.shape[0]
+            if self.use_dueling:  # Dueling DQN
+                q, v = self.Q(logits), self.V(logits)
+                if self.num_atoms > 1:
+                    q = q.view(bsz, -1, self.num_atoms)
+                    v = v.view(bsz, -1, self.num_atoms)
+                logits = q - q.mean(dim=1, keepdim=True) + v
+            elif self.num_atoms > 1:
+                logits = logits.view(bsz, -1, self.num_atoms)
+            if self.softmax:
+                logits = torch.softmax(logits, dim=-1)
+            return logits, state