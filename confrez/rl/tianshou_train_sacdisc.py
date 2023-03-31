7e507344a9541d6fece3fbaea98020863dd465f3
ayzhao7761@berkeley.edu <47766594+Enigma7761@users.noreply.github.com>
2023-03-30 20:03:35 -0700

    Small update to train and a sac discrete program, not fully working though

--- confrez/rl/tianshou_train_sacdisc.py
+++ confrez/rl/tianshou_train_sacdisc.py
@@ -7,22 +7,18 @@ from os import path as os_path
 import torch
 import numpy as np
 from scipy import interpolate
-from model import CNNDQN, DuelingDQN
-import gymnasium as gym
+from model import CNNDQN, Critic
 
-from tianshou.data import Collector, VectorReplayBuffer, PrioritizedVectorReplayBuffer
-from tianshou.env import DummyVectorEnv, PettingZooEnv, SubprocVectorEnv
+from tianshou.data import Collector, VectorReplayBuffer
+from tianshou.env import DummyVectorEnv, PettingZooEnv
 from torch.utils.tensorboard import SummaryWriter
 from tianshou.utils import TensorboardLogger
 
-from tianshou.policy import BasePolicy, DQNPolicy, MultiAgentPolicyManager
+from tianshou.policy import BasePolicy, DiscreteSACPolicy, MultiAgentPolicyManager
 from tianshou.trainer import offpolicy_trainer
-from tianshou.utils.net.common import Net
 
 from tianshou.utils import WandbLogger
-import wandb
 from torch.utils.tensorboard import SummaryWriter
-from tianshou_experiment import render
 
 cwd = os_path.dirname(__file__)
 now = datetime.now()
@@ -30,11 +26,10 @@ timestamp = now.strftime("%m-%d-%Y_%H-%M-%S")
 
 MODEL_NAME = "Tianshou-Multiagent"
 NUM_AGENT = 4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def step_schedule(
-    initial_value: float, steps: List[float], levels: List[float]
+        initial_value: float, steps: List[float], levels: List[float]
 ) -> Callable[[float], float]:
     """
     Step learning rate schedule.
@@ -58,53 +53,65 @@ def step_schedule(
     return func
 
 
-def get_env(render_mode="human"):
+def get_env():
     """This function is needed to provide callables for DummyVectorEnv."""
-    env = pklot_env.raw_env(
-        n_vehicles=NUM_AGENT, random_reset=False, seed=1, max_cycles=500, render_mode=render_mode
-    )  # seed=1
+    env = pklot_env.raw_env(n_vehicles=NUM_AGENT, random_reset=False, seed=1)  # seed=1
     env = ss.black_death_v3(env)
     env = ss.resize_v1(env, 140, 140)
     return PettingZooEnv(env)
 
 
-def get_agents() -> Tuple[BasePolicy, List[torch.optim.Optimizer], list]:
+def get_agents(
+        optim: Optional[torch.optim.Optimizer] = None,
+) -> Tuple[BasePolicy, torch.optim.Optimizer, list]:
     env = get_env()
     agents = []
-    optims = []
     for _ in range(NUM_AGENT):
-        net = DuelingDQN(
+        actor = CNNDQN(
             state_shape=(10, 3, 140, 140),
             action_shape=7,
             obs=env.observation_space.sample()[None],
             device="cuda" if torch.cuda.is_available() else "cpu",
         ).to("cuda" if torch.cuda.is_available() else "cpu")
 
-        optim = torch.optim.Adam(net.parameters(), lr=1e-4)  # , eps=1.5e-4
-        optims.append(optim)
+        critic1 = Critic(
+            state_shape=(10, 3, 140, 140),
+            obs=env.observation_space.sample()[None],
+            device="cuda" if torch.cuda.is_available() else "cpu",
+        ).to("cuda" if torch.cuda.is_available() else "cpu")
+
+        critic2 = Critic(
+            state_shape=(10, 3, 140, 140),
+            obs=env.observation_space.sample()[None],
+            device="cuda" if torch.cuda.is_available() else "cpu",
+        ).to("cuda" if torch.cuda.is_available() else "cpu")
+
+        optim1 = torch.optim.Adam(actor.parameters(), lr=1e-4)
+        optim2 = torch.optim.Adam(critic1.parameters(), lr=1e-4)
+        optim3 = torch.optim.Adam(critic2.parameters(), lr=1e-4)
 
         agents.append(
-            DQNPolicy(
-                model=net,
-                optim=optim,
-                discount_factor=0.9,
-                estimation_step=4,
-                target_update_freq=int(
-                    1000
-                ),
-            ).to("cuda" if torch.cuda.is_available() else "cpu")
+            DiscreteSACPolicy(
+                actor=actor,
+                actor_optim=optim1,
+                critic1=critic1,
+                critic1_optim=optim2,
+                critic2=critic2,
+                critic2_optim=optim3,
+                gamma=0.99,
+            )
         )
 
     policy = MultiAgentPolicyManager(agents, env)
-    return policy, optims, env.agents
+    return policy, optim, env.agents
 
 
 if __name__ == "__main__":
     # https://pettingzoo.farama.org/tutorials/tianshou/intermediate/
     # ======== Step 1: Environment setup =========
     # TODO: still don't quite get why we need dummy vectors
-    train_env = SubprocVectorEnv([get_env for _ in range(10)])
-    test_env = DummyVectorEnv([get_env for _ in range(1)])
+    train_env = DummyVectorEnv([get_env for _ in range(10)])
+    test_env = DummyVectorEnv([get_env for _ in range(10)])
 
     # seed
     seed = 42
@@ -119,73 +126,67 @@ if __name__ == "__main__":
     train_collector = Collector(
         policy,
         train_env,
-        # PrioritizedVectorReplayBuffer(100000, len(train_env), alpha=0.5, beta=0.4),
         VectorReplayBuffer(100000, len(train_env)),
         exploration_noise=True,
     )
     test_collector = Collector(policy, test_env)
-    for agent in agents:
-        policy.policies[agent].set_eps(1)
-    train_collector.collect(n_episode=50)  # batch size * training_num TODO
+    # policy.set_eps(0.2)
+    train_collector.collect(n_episode=20)  # batch size * training_num TODO
+
 
     # ======== Step 4: Callback functions setup =========
-    # logger:
-    logger = WandbLogger(project="confrez-tianshou", name=f"rainbow{timestamp}", save_interval=50)
-    script_path = os.path.dirname(os.path.abspath(__file__))
-    log_path = os.path.join(script_path, f"log/dqn/run{timestamp}")
-    writer = SummaryWriter(log_path)
-    # logger = TensorboardLogger(writer)
-    logger.load(writer)
-
 
     def save_best_fn(policy):
-        os.makedirs(os.path.join("log", "dqn"), exist_ok=True)
-        for i, agent in enumerate(agents):
-            model_save_path = os.path.join("log", "dqn", f"policy{i}.pth")
+        model_save_path = os.path.join("log", "rps", "dqn", "policy.pth")
+        os.makedirs(os.path.join("log", "rps", "dqn"), exist_ok=True)
+        for agent in agents:
             torch.save(policy.policies[agent].state_dict(), model_save_path)
-            logger.wandb_run.save(model_save_path)
-        render(agents, policy)
-        logger.wandb_run.log({'video': wandb.Video('out.gif', fps=4, format='gif')})
+        # TODO resolved
+
 
     def stop_fn(mean_rewards):
         # currently set to never stop
-        return mean_rewards >= 9950
+        return False
+
 
     def train_fn(epoch, env_step):
-        # print(env_step, policy.policies[agents[0]]._iter)
-        for agent in agents:
-            policy.policies[agent].set_eps(max(0.99**epoch, 0.1))
-            # train_collector.buffer.set_beta(min(0.4 * 1.02**epoch, 1))
+        pass
+
 
     def test_fn(epoch, env_step):
-        for agent in agents:
-            policy.policies[agent].set_eps(0.0)
-        if epoch % 50 == 0:
-            render(agents, policy)
-            logger.wandb_run.log({'video': wandb.Video('out.gif', fps=4, format='gif')})
+        pass
+
 
     def reward_metric(rews):
         return np.average(rews, axis=1)
 
+
+    # logger:
+    # script_path = os.path.dirname(os.path.abspath(__file__))
+    # log_path = os.path.join(script_path, f"log/dqn/run{timestamp}")
+    # writer = SummaryWriter(log_path)
+    # logger = TensorboardLogger(writer)
+    # logger = WandbLogger()
+    # logger.load(SummaryWriter("./log/"))
 
     # ======== Step 5: Run the trainer =========
     result = offpolicy_trainer(
         policy=policy,
         train_collector=train_collector,
         test_collector=test_collector,
-        max_epoch=10000,
-        step_per_epoch=500,
-        step_per_collect=10,
-        episode_per_test=1,
-        batch_size=32 * NUM_AGENT,
+        max_epoch=1000,
+        step_per_epoch=200,
+        step_per_collect=50,
+        episode_per_test=10,
+        batch_size=256,
         train_fn=train_fn,
         test_fn=test_fn,
         stop_fn=stop_fn,
         save_best_fn=save_best_fn,
-        update_per_step=0.1,
+        update_per_step=0.25,
         test_in_train=False,
         reward_metric=reward_metric,
-        logger=logger,
+        # logger=logger,
     )
 
     # return result, policy.policies[agents[1]]
